b7f71d43a249b549d179d7c038b7b2e1513d70ae
Buhay Malaya <malayabuhay@gmail.com>
2024-08-11 13:53:37 -0400

    edits pp rl

--- src/components/forms/SignupForm.jsx
+++ src/components/forms/SignupForm.jsx
@@ -78,12 +78,12 @@ export default function SignupForm() {
         <div className="about-decoy">
           <h6>
           
-By signing up for an account and clicking the "register" button below, you agree to both the Privacy Policy and Release of Liability as stipulated. <br /> <br />
+By signing up for an account and clicking the "register" button below, you agree to the terms of the Privacy Policy and Release of Liability. <br /> <br />
 
 <div className="about-decoy"> Privacy Policy <br /> <br />
 
 The [esc] Team ("we," "our," or "us") is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and protect the information you provide when using our services, 
-particularly in the context of domestic violence support and connecting you to respective DV shelters. <br />
+particularly in the context of domestic violence support and connecting you to respective DV shelters. <br /> <br />
 
 Information Collection and Use <br /> <br />
 
@@ -95,7 +95,6 @@ method of electronic storage, is 100% secure. <br /> <br />
 User Consent: By using our services, you consent to the collection and use of your information as outlined in this Privacy Policy. <br /> <br />
 Changes to This Policy: We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated effective date. <br /> <br />
 If you have any questions regarding this Privacy Policy, please contact us at esc.advocate@gmail.com.
-<br /> <br /> <br />
 
 </div> <br />
 
@@ -109,12 +108,11 @@ Technological Abuse <br /> <br />
 Awareness: Domestic violence abusers may use various forms of technological abuse, including but not limited to tracking your online activity, monitoring communications, 
 and accessing personal information through digital means. It is important to take precautions, such as using secure devices, using public access such as the library, 
 maintaining strong passwords, and clearing browsing history when necessary. <br /> <br />
-Limitation of Liability: You acknowledge that [Organization Name] provides tools and services to assist in situations of domestic violence, 
+Limitation of Liability: You acknowledge that [esc] provides tools and services to assist in situations of domestic violence, 
 but we cannot control or prevent all forms of technological abuse that may occur. By using our services, you agree to release the [esc] Team and its team members, employees, 
 and affiliates from any and all liability related to any misuse or abuse of your personal information by third parties, including abusers. <br /> <br />
 User Responsibility: It is your responsibility to use our services in a safe and secure manner. We recommend taking additional steps to protect your privacy 
 and safety when using our services, such as accessing our site from a secure location and using private browsing modes. <br /> <br />
-Agreement <br /> <br />
 
 By using our services, you agree to this Release of Liability. If you do not agree with these terms, please refrain from using our site and services. <br /> <br />
 
